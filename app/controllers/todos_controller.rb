122acc181a8bc7655aefd0fbe66f4a36bfd51578
Satwik Dash <satwikdash2000@gmail.com>
2021-05-27 14:10:54 +0530

    Can see a single todo in /todos/:id

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,6 +1,13 @@
 # todos_controller.rb
 class TodosController < ApplicationController
   def index
-    render plain: Todo.all.to_a
+    render plain: Todo.order(:id).map { |todo| todo.to_displayable_string }.join("\n")
+  end
+
+  def show
+    id = params[:id]
+    # render plain: Todo.where("id = ?",id).map { |todo| todo.to_displayable_string }
+    todo = Todo.find(id)
+    render plain: todo.to_displayable_string
   end
 end
--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,2 +1,6 @@
 class Todo < ActiveRecord::Base
+  def to_displayable_string
+    is_completed = completed ? "[X]" : "[ ]"
+    "#{id}. #{is_completed} #{todo_text} #{due_date}"
+  end
 end
