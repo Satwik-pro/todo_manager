c1085ce0662bc63f0de15a4b7bdf49e45f53bf38
Satwik Dash <satwikdash2000@gmail.com>
2021-05-27 14:45:07 +0530

    Added ability to create new todo in todos/create

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -10,4 +10,16 @@ class TodosController < ApplicationController
     todo = Todo.find(id)
     render plain: todo.to_displayable_string
   end
+
+  def create
+    todo_text = params[:todo_text]
+    due_date = Date.parse(params[:due_date])
+    new_todo = Todo.create!(
+      todo_text: todo_text,
+      due_date: due_date,
+      completed: false,
+    )
+    response_text = "New Todo created with ID: #{new_todo.id}"
+    render plain: response_text
+  end
 end
