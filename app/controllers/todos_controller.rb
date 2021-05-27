2d716ae8c3105d8b8de1486c422e231e5ffcbe57
Satwik Dash <satwikdash2000@gmail.com>
2021-05-27 17:17:06 +0530

    Using the resources routing convention for Todos

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,5 +1,7 @@
 # todos_controller.rb
 class TodosController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def index
     render plain: Todo.order(:id).map { |todo| todo.to_displayable_string }.join("\n")
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get "todos", to: "todos#index"
-  get "todos/create", to: "todos#create"
-  get "todos/:id", to: "todos#show"
+  # get "todos", to: "todos#index"
+  # post "todos", to: "todos#create"
+  # get "todos/:id", to: "todos#show"
+  resources :todos
 end
