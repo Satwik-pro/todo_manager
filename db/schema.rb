d8bef3b51b77d954b0123127ffcc25a6fdde43a3
Satwik Dash <satwikdash2000@gmail.com>
2021-05-31 22:20:05 +0530

    Updated DB Migrations

--- db/schema.rb
+++ db/schema.rb
@@ -25,12 +25,12 @@ ActiveRecord::Schema.define(version: 2021_05_31_054736) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
     t.string "first_name"
     t.string "last_name"
+    t.string "email"
     t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
